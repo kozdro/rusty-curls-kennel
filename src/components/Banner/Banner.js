0307bbffcad18736d35a236ae971cfbf29346821
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-05-24 17:24:47 +0200

    add slideshow

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,41 @@
-import style from "./Banner.module.css";
+import "react-slideshow-image/dist/styles.css";
+import "./Banner.css";
+import { Slide } from "react-slideshow-image";
+
+const slideImages = [
+  "https://i.ibb.co/dgC5rbv/IMG-0852.jpg",
+  "https://i.ibb.co/b7ymBPp/IMG-0853.jpg",
+  "https://i.ibb.co/NtHqwcp/IMG-0863.jpg",
+];
 
 function Logo() {
   return (
-    <div className={style.banner}>
-      <h1>Hodowla płowych pudli toy - Rusty Curls</h1>
-      banner
-    </div>
+    <>
+      <div className="slide-container">
+        <h1>
+          Hodowla płowych pudli toy
+          <br />
+          <span>Rusty Curls</span>
+        </h1>
+        <Slide>
+          <div className="each-slide">
+            <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
+              <span>Roy Rusty Curls</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{ backgroundImage: `url(${slideImages[1]})` }}>
+              <span>Roy Rusty Curls</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+              <span>Roy Rusty Curls</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    </>
   );
 }
 

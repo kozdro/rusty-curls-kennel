1339f189b680b96026e82712709e9ad9b54c7d6a
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-05-24 21:57:11 +0200

    set scroll fn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import Menu from "./Menu/Menu";
 import style from "./Header.module.css";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 80);
+    });
+  }, []);
+
   return (
-    <header className={style.header}>
+    <header
+      className={style.header}
+      style={scroll ? { backgroundColor: "rgba(5, 5, 23, 0.5)" } : {}}
+    >
       <Menu />
     </header>
   );

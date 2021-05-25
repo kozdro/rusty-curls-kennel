fa414b5cabbbc86ab404cd8a13be3d4694a22b22
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-05-25 18:48:09 +0200

    change import

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -1,5 +1,5 @@
 import style from "./Menu.module.css";
-import logo from "../../../assets/images/logo transparent.png";
+import logo from "../../../assets/images/logos/logo transparent.png";
 
 function Menu() {
   return (

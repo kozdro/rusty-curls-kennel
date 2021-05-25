668c9a5d823db8cfc1d12ee5ed6b6d567dbceabc
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-05-25 18:48:47 +0200

    add photoswipe and flex-containers

--- src/components/Kennel/Kennel.js
+++ src/components/Kennel/Kennel.js
@@ -1,4 +1,6 @@
 import style from "./Kennel.module.css";
+import puppies from "../../assets/images/litters/IMG_8260.JPG";
+import Photoswipe from "./Gallery/Photoswipe";
 
 function Kennel() {
   return (
@@ -13,11 +15,23 @@ function Kennel() {
           hodowlaną - Rea Wisdom and Beauty.
         </p>
       </section>
+      <Photoswipe />
       <section className={style.litters}>
         <h2 className={style.litters_title}>Nasze mioty</h2>
-        <p className={style.litters_subtitle}>
-          Zapraszamy do zapoznania się z naszymi miotami.
-        </p>
+        <div className={style.litters_container}>
+          <div className={style.litters_content_container}>
+            <p className={style.litters_subtitle}>
+              Zapraszamy do zapoznania się
+              <br />z naszymi miotami.
+            </p>
+            <button className={style.litters_subtitle_btn}>
+              obecne oraz planowane mioty
+            </button>
+          </div>
+          <div className={style.litters_img_container}>
+            <img src={puppies} alt="puppies" className={style.litters_img} />
+          </div>
+        </div>
       </section>
     </>
   );

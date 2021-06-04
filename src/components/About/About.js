2b77da061f87895f1251cff7f9236093c3f89db8
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-06-04 19:07:26 +0200

    add component

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import style from "./About.module.css";
+
+function About() {
+  return (
+    <div className={style.about}>
+      <h2 className={style.about_title}>O hodowli i o nas</h2>
+    </div>
+  );
+}
+
+export default About;

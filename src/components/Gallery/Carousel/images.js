41994cc48ebec206bcc5ca0c17f174531c3ea69c
Bartek Kozdrowiecki <bartek.kozdro@gmail.com>
2021-06-24 20:02:10 +0200

    add images.js

---
+++ src/components/Gallery/Carousel/images.js
@@ -0,0 +1,59 @@
+import img from "../../../assets/images/gallery2/IMG_9827.JPEG";
+import img2 from "../../../assets/images/gallery2/IMG_0414.JPEG";
+import img3 from "../../../assets/images/gallery2/IMG_0428.JPEG";
+import img4 from "../../../assets/images/gallery2/IMG_0743.JPG";
+import img5 from "../../../assets/images/gallery2/IMG_0821.JPEG";
+import img6 from "../../../assets/images/gallery2/IMG_1121.JPG";
+import img7 from "../../../assets/images/gallery2/IMG_1267.JPEG";
+import img8 from "../../../assets/images/gallery2/IMG_2292.jpg.jpg";
+import img9 from "../../../assets/images/gallery2/IMG_2681.JPEG";
+import img10 from "../../../assets/images/gallery2/IMG_4236.JPEG";
+import img11 from "../../../assets/images/gallery2/IMG_4404.JPEG";
+import img12 from "../../../assets/images/gallery2/IMG_4892.JPEG";
+import img13 from "../../../assets/images/gallery2/IMG_5737.JPEG";
+import img14 from "../../../assets/images/gallery2/IMG_6187.JPEG";
+import img15 from "../../../assets/images/gallery2/IMG_6552.JPEG";
+import img16 from "../../../assets/images/gallery2/IMG_6918.JPEG";
+import img17 from "../../../assets/images/gallery2/IMG_7041.JPEG";
+import img18 from "../../../assets/images/gallery2/IMG_7133.JPEG";
+import img19 from "../../../assets/images/gallery2/IMG_8254.JPEG";
+import img20 from "../../../assets/images/gallery2/IMG_8503.JPG";
+import img21 from "../../../assets/images/gallery2/IMG_8538.JPEG";
+import img22 from "../../../assets/images/gallery2/IMG_9091.JPEG";
+import img23 from "../../../assets/images/gallery2/IMG_9201.JPEG";
+import img24 from "../../../assets/images/gallery2/IMG_9248.JPEG";
+import img25 from "../../../assets/images/gallery2/IMG_9262.JPG";
+import img26 from "../../../assets/images/gallery2/IMG_9615.JPEG";
+import img27 from "../../../assets/images/gallery2/IMG_0088.JPEG";
+
+const images = {
+  img,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+};
+
+export default images;
